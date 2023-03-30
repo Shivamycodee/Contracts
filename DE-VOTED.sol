6cca3801d2ea235dae7d1ddccf336d98c81950de
shivam yadav <91057727+Shivamycodee@users.noreply.github.com>
2023-03-30 17:29:23 +0530

    Update DE-VOTED.sol

--- DE-VOTED.sol
+++ DE-VOTED.sol
@@ -36,7 +36,9 @@ contract devote is Ownable{
     event electionCreated(uint electionId,uint startDate,uint endDate);
     event candidateAdded(address indexed,uint enrollNo);
     event subAdminAdded(uint id,uint enrNo);
-    event voterAdded(address indexed, uint enrNo);
+    event voterAdded(address indexed, uint enrNo,uint sem);
+    event voted(uint,address,address);
+
 
     uint fiveMonths = 13149000;
 
@@ -52,7 +54,6 @@ contract devote is Ownable{
     struct voter{
         address voterAddress;
         uint  enrollNo;
-        bool hasVoted;
         uint year;
         uint sem;    
     }
@@ -87,6 +88,8 @@ contract devote is Ownable{
     mapping(uint =>mapping(address=>bool)) public hasVoted;
     mapping(address => uint) public access;
 
+    mapping(uint =>mapping(uint => voter)) public voterList;
+
     modifier onlySubAdmin(){
         require(subAdmins[msg.sender].isAllowed,"Only allowed sub-admins can access...");
         _;
@@ -109,6 +112,11 @@ contract devote is Ownable{
 
     modifier candidateExist(address _cad){
         require(candidates[_cad].add == msg.sender,"Candidate does not exist");
+        _;
+    }
+
+    modifier isEnded(uint id){
+        require(elections[id].end < block.timestamp,"Election is still going on...");
         _;
     }
 
@@ -148,9 +156,9 @@ contract devote is Ownable{
     }
 
    function createVoter(uint _enroll,uint _year,uint _sem) public onlyOwner onlySubAdmin{
-       voters[msg.sender] = voter(msg.sender,_enroll,false,_year,_sem);
+       voters[msg.sender] = voter(msg.sender,_enroll,_year,_sem);
        grantAccess(_year,_sem);
-       emit voterAdded(msg.sender,_enroll);
+       emit voterAdded(msg.sender,_enroll,_sem);
    }
 
    function grantAccess(uint _year,uint _sem) public {
@@ -164,11 +172,31 @@ contract devote is Ownable{
 
    function vote(uint _electionId,address _candidate) public isElectionAlive(elections[_electionId]) isAuthorized() candidateExist(_candidate){
        require(!hasVoted[_electionId][_candidate],"you have already voted...");
+
          uint enr = voters[msg.sender].enrollNo;
+
          myVote[_electionId][enr] = _candidate;
+
          ballotCount[_candidate][_electionId] += 1;
          elections[_electionId].voteCount += 1;
+
          hasVoted[_electionId][_candidate] = true;
+
+         uint voterId = elections[_electionId].voteCount;
+         voterList[_electionId][voterId] = voters[msg.sender];
+
+         emit voted(_electionId,msg.sender,_candidate);
+   }
+
+   function getElectionData(uint _id) public view isEnded(_id) returns(uint,uint,string memory,string memory,string memory,uint,voter[] memory){
+       election memory elc = elections[_id];
+       uint totalVotes = elc.voteCount;
+
+       voter[] memory vt = new voter[](totalVotes);
+       for(uint i=1; i <= totalVotes ;i++){
+           vt[i] = voterList[_id][i];         
+       }
+       return (elc.start,elc.end,elc.department,elc.position,elc.description,elc.voteCount,vt);
    }
 
 
